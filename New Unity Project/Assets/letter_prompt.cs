373d237cd307804b277214aaae7988303787f4bb
Lawrence <lawrence.ora@mail.utoronto.ca>
2020-01-11 17:44:59 -0500

    added letter_prompt script

---
+++ New Unity Project/Assets/letter_prompt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class letter_prompt : MonoBehaviour
+{
+
+	public Image currImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//currImage = ButtonPrompt.Load("a.png");	        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currImage = ButtonPrompt.Load("q.png");
+		//currImage = ButtonPrompt.Load("z.png");
+    }
+}
