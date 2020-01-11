ec245c626880f1ac881fef14cd525c4801647be7
Lawrence <lawrence.ora@mail.utoronto.ca>
2020-01-11 17:46:08 -0500

    fixed letter_prompt

--- New Unity Project/Assets/letter_prompt.cs
+++ New Unity Project/Assets/letter_prompt.cs
@@ -5,18 +5,13 @@ using UnityEngine.UI;
 
 public class letter_prompt : MonoBehaviour
 {
-
-	public Image currImage;
     // Start is called before the first frame update
     void Start()
     {
-		//currImage = ButtonPrompt.Load("a.png");	        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //currImage = ButtonPrompt.Load("q.png");
-		//currImage = ButtonPrompt.Load("z.png");
     }
 }
